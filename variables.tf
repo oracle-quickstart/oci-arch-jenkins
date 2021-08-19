d9e635976e82b43766dc9ccf88c48eb0a48b67e1
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-08-19 14:45:16 +0200

    Updated provisioner and variables tf files

--- variables.tf
+++ variables.tf
@@ -4,9 +4,9 @@
 variable "tenancy_ocid" {}
 variable "region" {}
 variable "compartment_ocid" {}
-#variable "user_ocid" {}
-#variable "fingerprint" {}
-#variable "private_key_path" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
 variable "availablity_domain_name" {}
 
 variable "release" {
